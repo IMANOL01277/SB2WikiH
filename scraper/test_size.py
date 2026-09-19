e7a0ee859c6b5180a4b9d7074ff5b69ebafce256
imanol <imanolmendoza2012@gmail.com>
2026-09-18 21:51:41 -0500

    Upd 123

---
+++ scraper/test_size.py
@@ -0,0 +1,10 @@
+﻿import csv
+with open("SB2ItemDB.csv", encoding="utf-8") as f:
+    data = f.read()
+    
+# Count how much we save by stripping the base url
+reduced = data.replace("https://swordburst2.fandom.com/Special:Redirect/file/", "")
+reduced = reduced.replace("https://swordburst2.fandom.com/wiki/", "")
+
+print(f"Original size: {len(data)}")
+print(f"Reduced size: {len(reduced)}")
